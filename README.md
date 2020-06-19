33bdf1c7fe943ac2d8e793ca78e28ce1e3b06f30
Johannes Künsebeck <jkuensebck@taz.de>
2020-06-19 18:25:37 +0200

    Empty package / setuptools / LICENSE / README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# pyvoussoir
+Automatic de-keystoning/page-splitting tool for single camera book scanners, python port of [voussoir](https://github.com/publicus/voussoir). 
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+install_requires = open('requirements.txt').read().split('\n')
+
+setup(
+    name='pyvoussoir',
+    version='0.2',
+    author='Johannes Künsebeck',
+    author_email='kuensebeck@googlemail.com',
+    description='Automatic de-keystoning/page-splitting tool for single camera book scanners',
+    license='Apache License 2.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/hnesk/pyvoussoir",
+    packages=['voussoir'],
+    install_requires = install_requires,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Topic :: Scientific/Engineering :: Image Recognition'
+    ],
+    keywords=['OCR', 'page splitting', 'page dewarping', 'perspective correction', 'voissour']
+
+)
